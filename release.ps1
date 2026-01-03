66e43013bd393e0b9e1143d90bea56340294e666
Marcelo <msahardware@hotmail.com>
2026-01-03 07:32:39 -0300

    release: v1.0.0

---
+++ release.ps1
@@ -0,0 +1,9 @@
+﻿param([string]$version)
+
+git add .
+git commit -m "release: v$version"
+git tag -a "v$version" -m "Release v$version"
+git push origin main
+git push origin "v$version"
+
+Write-Host "✅ Versão v$version publicada!" -ForegroundColor Green
